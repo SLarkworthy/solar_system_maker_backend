2291b9a414d0b0a6c82718a79edb63fb8408da8f
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-06 14:05:35 -0400

    solar system model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+s1 = SolarSystem.create(name: "sol1")
+s2 = SolarSystem.create(name: "sol2")
