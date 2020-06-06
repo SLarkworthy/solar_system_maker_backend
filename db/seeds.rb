62d6d25d3aeb63829dd6ec8eb5a7025d11fadfa7
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-06 15:01:25 -0400

    star model added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Star.create(name: "biglight", spectral_type: "O", solar_system_id: 1)
