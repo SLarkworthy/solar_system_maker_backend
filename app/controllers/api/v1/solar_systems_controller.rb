b785feb492c04d19f1aced4408374c300356a7dc
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-08 14:06:26 -0400

    solar system index and create

--- app/controllers/api/v1/solar_systems_controller.rb
+++ app/controllers/api/v1/solar_systems_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::SolarSystemsController < ApplicationController
+
+    def index 
+        solar_systems = SolarSystem.all
+        render json: solar_systems
+    end
+
+    def create
+        solar_system = SolarSystem.new(solar_system_params)
+        if solar_system.save
+            render json: solar_system, status: :accepted
+        else
+            render json: {errors: solar_system.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+        def solar_system_params
+            params.require(:solar_system).permit(:name)
+        end
+
 end
